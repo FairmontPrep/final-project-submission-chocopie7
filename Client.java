fa5454b722abed5a76ab9b89c0fe3584fb539a97
Ryan <142356165+chocopie7@users.noreply.github.com>
2025-05-20 10:23:40 -0700

    Commit 1

--- Client.java
+++ Client.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 public class Client{
     public static void main(String[] args){
         ArrayList<ArrayList<Integer>> test_array_2 = new ArrayList<>();
@@ -17,4 +18,28 @@ public class Client{
         }
 
     }
-}
+    public ArrayList<int[]> findStart(ArrayList<ArrayList<Integer>> input){
+        int row = input.get(0).size();
+        int column = input.size();
+        ArrayList<int[]> returnarray = new ArrayList<int[]>();
+        for(int i=0;i<row;i+=row-1){
+            for(int j=0;j<column;j+=column-1){
+                if(input.get(i).get(j)==1){
+                    returnarray.add(new int[] {i,j});
+                }
+            }
+        }
+        return returnarray;
+    }
+    public ArrayList<int[]> findroute(ArrayList<int[]> start){
+        for (int[] pos:start){
+            ArrayList<int[]> path = new ArrayList<>();
+            while (path(pos)){
+                path.add(pos);
+                pos=
+            }
+        else return findroute(
+            (input)->{return {input[0]+1, input[1]}, input})||findroute(input+1)
+    public int[] next(int[]){
+        int
+    }
